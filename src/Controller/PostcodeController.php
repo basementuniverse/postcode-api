75d6c88cec8c3e2e253994a57015a23392bd069f
Gordon <gordonlarrigan@gmail.com>
2021-03-21 21:42:39 +0000

    Controller and repository initial implementation

--- src/Controller/PostcodeController.php
+++ src/Controller/PostcodeController.php
@@ -2,25 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\PostcodeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostcodeController extends AbstractController
 {
-    public function findByPartialMatch(string $partial): Response
-    {
-        return $this->json([
-            'partial' => $partial,
-        ]);
+    public function findByPartialMatch(
+        PostcodeRepository $repository,
+        string $partial
+    ): Response {
+        return $this->json($repository->findByPartialMatch($partial));
     }
 
-    public function findByLocation(float $lat, float $long, float $range): Response
-    {
-        return $this->json([
-            'lat' => $lat,
-            'long' => $long,
-            'range' => $range,
-        ]);
+    public function findByLocation(
+        PostcodeRepository $repository,
+        float $lat,
+        float $long,
+        float $range
+    ): Response {
+        return $this->json($repository->findByLocation($lat, $long, $range));
     }
 }
--- src/Repository/PostcodeRepository.php
+++ src/Repository/PostcodeRepository.php
@@ -7,44 +7,103 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method Postcode|null find($id, $lockMode = null, $lockVersion = null)
- * @method Postcode|null findOneBy(array $criteria, array $orderBy = null)
- * @method Postcode[]    findAll()
- * @method Postcode[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Postcode[] findByPartialMatch(string $partial)
+ * @method Postcode[] findByLocation(float $lat, float $long, float $range)
  */
 class PostcodeRepository extends ServiceEntityRepository
 {
+    private const EARTH_RADIUS              = 6371; // Radius of earth in km
+    private const KM_PER_DEGREE_LATITUDE    = 111;  // Km in a degree of latitude
+    private const KM_PER_DEGREE_LONGITUDE   = 111;  // Km in a degree of longitude at the equator
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Postcode::class);
     }
 
-    // /**
-    //  * @return Postcode[] Returns an array of Postcode objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @param string $partial
+     *
+     * @return Postcode[]
+     */
+    public function findByPartialMatch(string $partial): array
     {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
+        return $this->createQueryBuilder('postcode')
+            ->andWhere('postcode.postcode LIKE :partial')
+            ->setParameter('partial', "%$partial%")
+            ->orderBy('postcode.postcode', 'ASC')
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
     }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Postcode
+    /**
+     * @param float $lat
+     * @param float $long
+     * @param float $range
+     *
+     * @return Postcode[]
+     */
+    public function findByLocation(float $lat, float $long, float $range): array
     {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        // Broad phase
+        // Calculate latitude/longitude offsets for the top-left and bottom-right corners of the search area
+        $latitudeOffset = $range / self::KM_PER_DEGREE_LATITUDE;
+        $longitudeOffset = $range / $this->calculateKmPerDegreeLongitude($lat);
+        $top = $lat - $latitudeOffset;
+        $left = $long - $longitudeOffset;
+        $bottom = $lat + $latitudeOffset;
+        $right = $long + $longitudeOffset;
+
+        // Find all postcodes in the search area
+        // ...
+
+        // Narrow phase
+        // Filter results within search radius
+
+        return [];
     }
-    */
+
+    /**
+     * Calculate the distance between points a and b in km
+     * https://en.wikipedia.org/wiki/Haversine_formula
+     *
+     * Doesn't take into account the ellipsoidal shape of the Earth - will overestimate transpolar distances
+     * and underestimate transequitorial distances.
+     *
+     * @param float $latitudeA
+     * @param float $longitudeA
+     * @param float $latitudeB
+     * @param float $longitudeB
+     *
+     * @return float
+     */
+    private function calculateDistance(
+        float $latitudeA,
+        float $longitudeA,
+        float $latitudeB,
+        float $longitudeB
+    ): float {
+        $phi1 = deg2rad($latitudeA);
+        $phi2 = deg2rad($latitudeB);
+        $deltaLatitude = deg2rad($latitudeB - $latitudeA);
+        $deltaLongitude = deg2rad($longitudeB - $longitudeA);
+
+        $a = sin($deltaLatitude / 2) ** 2 + cos($phi1) * cos($phi2) * sin($deltaLongitude / 2) ** 2;
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+
+        return self::R * $c;
+    }
+
+    /**
+     * Calculate how many km in a degree of longitude at a specific latitude. Latitude degrees
+     * are parallel, but longitude degrees converge to 0 at the poles.
+     *
+     * @param float $longitude
+     *
+     * @return float
+     */
+    private function calculateKmPerDegreeLongitude(float $latitude): float
+    {
+        return cos(deg2rad($latitude)) * self::KM_PER_DEGREE_LONGITUDE;
+    }
 }
